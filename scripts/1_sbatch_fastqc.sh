1d2d04d659d27fc30f7db91dda83e332f79b7d5e
soom12 <soo.m@northeastern.edu>
2024-12-19 16:33:23 -0500

    tweak script 1

--- scripts/1_sbatch_fastqc.sh
+++ scripts/1_sbatch_fastqc.sh
@@ -25,7 +25,9 @@ echo "Fastqc output: " $FASTQC_OUT_DIR
 mkdir -p $FASTQC_OUT_DIR
 
 echo "Running fastqc on files listed in $FASTQ_FILE_LIST"
-while read file; do fastqc $file; done <<<"$FASTQ_FILE_LIST"
+paste $FASTQ_FILE_LIST | while read file; do
+  fastqc $file
+done
 
 echo "Cleaning up logs and output files"
 
